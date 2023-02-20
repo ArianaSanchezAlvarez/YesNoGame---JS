7fb825ef1fd4ef5653b400389fe5ea4e4efffcdf
Ariana Sánchez Álvarez <saaigj@gmail.com>
2023-02-20 09:59:57 -0700

    Calling the API

--- starter-files/script.js
+++ starter-files/script.js
@@ -11,3 +11,10 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+
+const fetchAnswer = () => {
+    fetch(API_ENDPOINT)
+    .then(data => data.json())
+    .then(data => console.log(data));
+};
+fetchAnswer();
